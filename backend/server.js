98487dc385387bd7b6afea66519c287010486fa7
satyajit-patel <satyajitpatel770001@gmail.com>
2025-03-15 18:14:51 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(
+  cors({
+    origin: "https://tle-contest-tracker-api.vercel.app", // Allow specific origin
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, // If using cookies or authentication
+  })
+);
 app.use(cors());
 app.use(express.json());
 
